19cd89ca9355ba2a75b185361e47b28fa14ad057
John Doe <you@example.com>
2022-06-20 17:15:04 +0300

    На проверку

--- test_items.py
+++ test_items.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 
 
 
+
 def test_add_to_shopping_cart(browser):
     browser.get( "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     time.sleep(30)
